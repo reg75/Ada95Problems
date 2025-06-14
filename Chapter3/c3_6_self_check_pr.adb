768fae1fd1dbf969afec8ece5f48d50e4d45c3ec
Paul Regnier <paul_m_regnier@yahoo.com>
2025-06-14 09:03:44 +0100

    Add solutions for 3.6 self-check (assingment syntax)

---
+++ Chapter3/c3_6_self_check_pr.adb
@@ -0,0 +1,19 @@
+with Ada.Integer_Text_IO;
+procedure c3_6_self_check_pr is
+
+-- Solutions for 3.6 Self-Check exercises
+-- Author: Paul Regnier | Last Modified: June 2025
+   
+   -- X = Y; Invalid. Incorrect assignment operator
+   -- A := B - C; Valid. Arithmetic assignment. Assumes B & C have been declared.
+   -- P + Q := R; Invalid. P + Q is an expression, not a variable. 
+   -- G := G; Valid - Value of G remains unchanged. Assumes G been declared.
+   -- H := 3 + 4; Valid. H is assigned the value of adding 3 + 4
+   -- H := 3 + K; Valid. Assumes K has been declared.
+   -- T := S * T; Valid. Assumes S & T have been declared.
+
+begin
+   
+   null;
+
+end c3_6_self_check_pr;
