fb1ab6ed8821d7cd7439ed4e60d9eaabd2abe78b
Paul Regnier <paul_m_regnier@yahoo.com>
2025-06-21 08:01:15 +0100

    Updated solutions to 3.8 self-check exercises.

--- Chapter3/c3_selfcheck_3_8.adb
+++ Chapter3/c3_selfcheck_3_8.adb
@@ -18,6 +18,9 @@ x : Float;
 y : Float := -1.0;
 i : Integer;
 
+color, lime, straw, red, orange : Integer;
+black, white, green, purple, crayon : Float;
+
 
 begin
    
@@ -25,36 +28,37 @@ begin
    Ada.Text_IO.Put_Line(Item => "Question 1:");
    Ada.Integer_Text_IO.Put(Item => 2 / 7); -- 0
    Ada.Integer_Text_IO.Put(Item => 7 / 22); -- 0
-   Ada.Integer_Text_IO.Put(Item => 22 REM 7); -- 1
-   Ada.Integer_Text_IO.Put(Item => 7 REM 22); -- 7
+   Ada.Integer_Text_IO.Put(Item => 22 rem 7); -- 1
+   Ada.Integer_Text_IO.Put(Item => 7 rem 22); -- 7
    Ada.Text_IO.New_Line;
 
    Ada.Integer_Text_IO.Put(Item => 15 / 16); -- 0
    Ada.Integer_Text_IO.Put(Item => 16 / 15); -- 1
-   Ada.Integer_Text_IO.Put(Item => 16 REM 15); -- 1
-   Ada.Integer_Text_IO.Put(Item => 15 REM 16); -- 15
+   Ada.Integer_Text_IO.Put(Item => 16 rem 15); -- 1
+   Ada.Integer_Text_IO.Put(Item => 15 rem 16); -- 15
    Ada.Text_IO.New_Line;
 
    Ada.Integer_Text_IO.Put(Item => 3 / 23); -- 0
    Ada.Integer_Text_IO.Put(Item => 23 / 3); -- 7
-   Ada.Integer_Text_IO.Put(Item => 3 REM 23); -- 3
-   Ada.Integer_Text_IO.Put(Item => 23 REM 3); -- 2
+   Ada.Integer_Text_IO.Put(Item => 3 rem 23); -- 3
+   Ada.Integer_Text_IO.Put(Item => 23 rem 3); -- 2
    Ada.Text_IO.New_Line;
 
    Ada.Integer_Text_IO.Put(Item => 4 / 16); -- 0
    Ada.Integer_Text_IO.Put(Item => 16 / 4); -- 4
-   Ada.Integer_Text_IO.Put(Item => 4 REM 16); -- 4
-   Ada.Integer_Text_IO.Put(Item => 16 REM 4); -- 0
-   Ada.Text_IO.New_Line;
+   Ada.Integer_Text_IO.Put(Item => 4 rem 16); -- 4
+   Ada.Integer_Text_IO.Put(Item => 16 rem 4); -- 0
+   Ada.Text_IO.New_Line(2);
 
    -- Question 2 Solutions
-   i := A REM B; -- a) Valid
+   Ada.Text_IO.Put_Line(Item => "Question 2:");
+   i := A rem B; -- a) Valid
    Ada.Integer_Text_IO.Put(Item => i, Width => 1); -- 3
    Ada.Text_IO.New_Line;
    i := (990 - MAXI) / A; -- b) Valid
    Ada.Integer_Text_IO.Put(Item => i, Width => 0); -- -3
    Ada.Text_IO.New_Line;
-   -- I := a REM y; -- c) Invalid (Integer and Float types)
+   -- I := a rem y; -- c) Invalid (Integer and Float types)
    x := PI * y; -- d) Valid
    Ada.Float_Text_IO.Put (Item => x, Fore => 1, Aft => 2, Exp => 0); -- -3.14
    Ada.Text_IO.New_Line;
@@ -62,12 +66,93 @@ begin
    Ada.Integer_Text_IO.Put(Item => i, Width => 1); -- 0
    Ada.Text_IO.New_Line;
    -- x := a / b; f) Invalid (assigning Integer division to a Float)
-   -- x := a REM (a / b); g) Invalid (assinging Integer to Float variable)
-   i := b  / 0; -- h) Invalid (division by zero)
-   i := a REM (990 - MAXI); -- i) Valid
+   -- x := a rem (a / b); g) Invalid (assinging Integer to Float variable)
+   -- i := b  / 0; -- h) Invalid (division by zero)
+   i := a rem (990 - MAXI); -- i) Valid
    Ada.Integer_Text_IO.Put(Item => i, Width => 1); -- 3
    Ada.Text_IO.New_Line;
-   i := (MAXI - 990) / a; -- 
+   i := (MAXI - 990) / a; -- j) Valid
+   Ada.Integer_Text_IO.Put(Item => i, Width => 1); -- 3
+   Ada.Text_IO.New_Line;
+   --x := a / y; -- k) Invalid (division operation combines Integer and Float types)
+   x := PI ** 2; -- l) Valid
+   Ada.Float_Text_IO.Put (Item => x, Fore => 1, Aft => 2, Exp => 0); -- 9.87
+   Ada.Text_IO.New_Line;
+   -- x := PI ** y; -- m) Invalid (exponent must be a Natural number)
+   -- x := a / b; -- n) Invalid (assigning Integer division to a Float)
+   i := (MAXI - 990) rem a; -- o) Valid
+   Ada.Integer_Text_IO.Put(Item => i, Width => 1); -- 1
+   Ada.Text_IO.New_Line;
+   -- i := a rem 0; -- p) Invalid using rem with zero divisor
+   i := a rem (MAXI - 999); -- q) Valid
+   Ada.Integer_Text_IO.Put(Item => i, Width => 1); -- 0
+   Ada.Text_IO.New_Line(2);
+
+   -- Question 3
+   Ada.Text_IO.Put_Line (Item => "Question 3:");
+   
+   -- Reassinging variables
+   a := 5;
+   b := 2;
+   y := 2.0;
+
+   -- Question 3 Solutions
+   i := a rem b; -- a) 
+   Ada.Integer_Text_IO.Put(Item => i, Width => 1); -- 1
+   Ada.Text_IO.New_Line;
+   i := (990 - MAXI) / a; -- b)
+   Ada.Integer_Text_IO.Put(Item => i, Width => 0); -- -2
+   Ada.Text_IO.New_Line;
+   x := PI * y; -- d) Valid
+   Ada.Float_Text_IO.Put (Item => x, Fore => 1, Aft => 2, Exp => 0); -- 6.28
+   Ada.Text_IO.New_Line;
+   i := a / b; -- e) Valid
+   Ada.Integer_Text_IO.Put(Item => i, Width => 1); -- 2
+   Ada.Text_IO.New_Line;
+   i := a rem (990 - MAXI); -- i) Valid
+   Ada.Integer_Text_IO.Put(Item => i, Width => 1); -- 5
+   Ada.Text_IO.New_Line;
+   i := (MAXI - 990) / a; -- j) Valid
+   Ada.Integer_Text_IO.Put(Item => i, Width => 1); -- 2
+   Ada.Text_IO.New_Line;
+   x := PI ** 2; -- l) Valid
+   Ada.Float_Text_IO.Put (Item => x, Fore => 1, Aft => 2, Exp => 0); -- 9.87
+   Ada.Text_IO.New_Line;
+   i := (MAXI - 990) rem a; -- o) Valid
+   Ada.Integer_Text_IO.Put(Item => i, Width => 1); -- 0
+   Ada.Text_IO.New_Line;
+   i := a rem (MAXI - 999); -- q) Valid
+   Ada.Integer_Text_IO.Put(Item => i, Width => 1); -- 0
+   Ada.Text_IO.New_Line(2);
+
+   -- Question 4
+   Ada.Text_IO.Put_Line (Item => "Question 4:");
+   
+   -- Assinging variables
+   color := 2;
+   straw := 1;
+   red := 3;
+   black := 2.5;
+   crayon := -1.3;
+   purple := 0.3E1;
+
+   -- Question 4 Solutions
+   white := crayon * 2.5 / purple; -- a)
+   Ada.Float_Text_IO.Put (Item => white, Fore => 1, Aft => 2, Exp => 0); -- -1.08
+   Ada.Text_IO.New_Line;
+   green := black / purple; -- b)
+   Ada.Float_Text_IO.Put (Item => green, Fore => 1, Aft => 2, Exp => 0); -- 0.83
+   Ada.Text_IO.New_Line;
+   orange := color / red; -- c)
+   Ada.Integer_Text_IO.Put(Item => orange, Width => 0); -- 0
+   Ada.Text_IO.New_Line;
+   orange := (color + straw) / (2 * straw); -- d)
+   Ada.Integer_Text_IO.Put(Item => orange, Width => 0); -- -1
+   Ada.Text_IO.New_Line;
+   lime := red / color + red rem color; -- e)
+   Ada.Integer_Text_IO.Put(Item => lime, Width => 0); -- 2
+   -- purple := straw / red * color; -- f) Invalid (assigning Integer to Float variable)
+   Ada.Text_IO.New_Line(2);
 
 
 end c3_selfcheck_3_8;
